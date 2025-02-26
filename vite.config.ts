7ff543de7f01068ad9e93381011eddf500dc31e2
takamatsu.asuka <takamatsu.asuka@tis.co.jp>
2025-02-26 15:25:51 +0900

    implement login UI & define default S3 bucket which this app creates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: false, // Disable CORS
+    headers: {
+      'Access-Control-Allow-Origin': 'null' // Enforce SOP
+    }
+  }
 })
